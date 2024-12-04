a1cd0ef784f0426666daf91445426ca3209094d5
Nicolas Jamar <32648286+NicolasJamar@users.noreply.github.com>
2024-12-04 08:58:38 +0100

    Update 08-01-task-manager.js

--- 08-01-task-manager.js
+++ 08-01-task-manager.js
@@ -1,4 +1,5 @@
 const readline = require('readline');
+const fs = require("fs");
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -6,9 +7,60 @@ const rl = readline.createInterface({
 });
 
 
-rl.question('Your choice: ', (answer) => {
-  console.log(answer)
-  rl.close();
-});
+function displayTasks() {
+  
+}
 
+function AddTask() {
+  rl.question('Your choice: ', (answer) => {
+   console.log(answer)
+  });
+}
 
+function DeleteTask() {
+  
+}
+
+function MarkAsDone() {
+  
+}
+
+// function SaveInJSON() {
+  //fs.writeFileSync("tasks.json", tasksJSON);
+// }
+
+// function LoadJSON() {
+    //fs.readFileSync("tasks.json", "utf8"
+// }
+
+const menu = () => {
+  return "Welcome to your task manager, Press:\n1. to see all your tasks\n2. to add a task\n3. to delete a task\n4. to mark a task as done\n5. to Exit the task manager\nYour choice: ";
+};
+
+function run() {
+  rl.question(menu(), (answer) => {
+    switch (answer) { 
+      case 1:
+
+        break;
+    case 2:
+
+        break;
+    case 3:
+
+        break;
+    case 4:
+
+        break;
+    case 5:
+        
+        rl.close();
+        break;
+    default:
+        console.log("Invalid choice");
+        console.clear();
+        run();
+  });
+}
+
+run()
